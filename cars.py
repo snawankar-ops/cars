25321bce1024fca9c974db2ce09021f3d3b2b7a6
Sarah <snawankar@gmail.com>
2025-12-27 01:20:01 +0530

    inheritance electric cars and sports cars

--- cars.py
+++ cars.py
@@ -44,3 +44,33 @@ car1.set_price(9000)
 #confirming change
 car1.get_info()
 
+#creating a subclass electriccar
+
+class ElectricCar(Cars):
+    #adding battery range
+    def __init__(self, make:str, model:str, price:float, battery_range:float):
+        super().__init__(make, model, price)
+        self.battery_range = battery_range
+
+    def get_info(self):
+        super().get_info()
+        print(f"battery range: {self.battery_range} miles")
+
+e1 = ElectricCar("tesla", "modelxyz", 50000, 23)
+e1.get_info()
+
+#creating a subclass sportscar
+
+class SportsCar(Cars):
+    def __init__(self, make:str, model:str, price:float, top_speed:float):
+        super().__init__(make, model, price)
+        self.top_speed = top_speed
+
+    def get_info(self):
+        super().get_info()
+        print(f"top speed is {self.top_speed} mph")
+
+s1 = SportsCar("porsche", "modelblabla", 900000, 78)
+s1.get_info()
+
+    
