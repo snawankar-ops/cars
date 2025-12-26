cb2b3b19bc294a050fd5a3b770b842517a634159
Sarah <snawankar@gmail.com>
2025-12-26 21:59:22 +0530

    adding getters and setters and making price private

--- cars.py
+++ cars.py
@@ -2,19 +2,28 @@ class Cars:
     pay_rate = 0.8 #after 20% discount
     def __init__(self, make: str, model: str, price:float):
 
-        assert price>=0 , f"price {price} is not greater than or equal to zero"
-
         self.make = make
         self.model = model
-        self.price = price
+        self.__price = price
 
     def apply_discount(self):
-        self.price = self.price * self.pay_rate
+        self.__price = self.__price * self.pay_rate
 
     def get_info(self):
         print(f"Make: {self.make}")
         print(f"Model: {self.model}")
-        print(f"Price: {self.price}")
+        print(f"Price: {self.__price}")
+
+#adding a getter
+    def get_price(self):
+        return self.__price
+    
+#adding a setter
+    def set_price(self, price):
+        if price >= 0:
+            self.__price = price
+        else:
+            print("price cannot be negative")
 
 car1 = Cars("Toyota", "corolla", 100000)
 car2 = Cars("Ford", "Mustang", 200000)
@@ -25,5 +34,13 @@ car2.apply_discount()
 car1.get_info()
 car2.get_info()
 
-print(f"The price of car1 is, {car1.price}")
-print(f"the price of car2 is, {car2.price}")
+#get price
+
+print(car1.get_price())
+
+#set price
+car1.set_price(9000)
+
+#confirming change
+car1.get_info()
+
