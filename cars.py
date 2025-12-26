6aa9a0d85ed44fbaa14199ce3eee6d311e7f771c
Sarah <snawankar@gmail.com>
2025-12-26 20:13:13 +0530

    creating classes, attributes and methods

---
+++ cars.py
@@ -0,0 +1,21 @@
+class Cars:
+    pay_rate = 0.8 #after 20% discount
+    def __init__(self, make: str, model: str, price:float):
+
+        assert price>=0 , f"price {price} is not greater than or equal to zero"
+
+        self.make = make
+        self.model = model
+        self.price = price
+
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+car1 = Cars("Toyota", "corolla", 100000)
+car2 = Cars("Ford", "Mustang", 200000)
+
+car1.apply_discount()
+car2.apply_discount()
+
+print(f"The price of car1 is, {car1.price}")
+print(f"the price of car2 is, {car2.price}")
